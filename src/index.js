c4b27b7d9beb05cc385a30b9ac1a2e7fde960952
Gabriel Araujo <gabriel.araujo@s2it.com.br>
2019-03-05 01:10:40 -0300

    [add] adding header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // JS Goes here - ES6 supported
+import "jquery";
+import "bootstrap/js/dist/collapse";
 
-import "./css/main.css";
+import "./css/main.scss";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
